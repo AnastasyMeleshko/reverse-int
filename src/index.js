65c07eb163021b5da8308a3e4b9f72a280660b7b
AnastasyMeleshko <7634907@mail.ru>
2022-01-08 19:42:35 +0300

    feat: add solution for task Reverse Int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    n = String(n);
+    let result = n.split("").reverse().join("");
+    if (result[result.length-1] === "-") {
+        return Number(result.slice(0,result.length-1));
+    }
+    return Number(result);
 }
